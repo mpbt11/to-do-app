00e62ff67551eb0643a69c54fc3117370e55f47a
mpbt11 <mariapaula.torres64@gmail.com>
2025-10-03 01:41:44 -0300

    feat: integrate Toaster component into RootLayout for improved notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/core/contexts/AuthContext";
 import { ThemeProvider } from "@/shared/components/theme-provider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <AuthProvider>
+            <Toaster />
             {children}
           </AuthProvider>
         </ThemeProvider>
